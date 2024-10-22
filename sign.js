6ef7e01a31c7d9f4117cb8f6977b8d6027206978
Nixonfernandes-18 <149685304+Nixonfernandes-18@users.noreply.github.com>
2024-10-22 12:23:55 +0530

    starting changes

---
+++ sign.js
@@ -0,0 +1,4 @@
+// JavaScript to handle redirection to the sign-up page when the "Sign Up" button is clicked
+document.getElementById('signup-btn').addEventListener('click', function() {
+    window.location.href = 'signup.html'; // Redirect to the sign-up page
+});
